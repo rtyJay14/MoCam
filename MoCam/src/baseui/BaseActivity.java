7d21fcd55489cf43c813da80bc7c86d3c96ee6e9
rtyJay14 <rtyJay14@gmail.com>
2014-09-03 00:43:50 -0700

    base UI

---
+++ MoCam/src/baseui/BaseActivity.java
@@ -0,0 +1,13 @@
+package asd;
+
+import android.app.Activity;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+/**
+ * @author Paresh Mayani (@pareshmayani)
+ */
+public abstract class BaseActivity extends Activity {
+
+	protected ImageLoader imageLoader = ImageLoader.getInstance();
+	
+}
